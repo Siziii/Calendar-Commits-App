64657f58013f803eda5f24f091c84dbd9a719ead
Siziii <sizi.dev@gmail.com>
2023-11-02 18:58:11 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="bg-bg1 w-screen h-screen flex items-center justify-center">
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors:{
+      primary:"#3B3B3B",
+      secondary:"#2b2b2b",
+      bg1:"#222222",
+      accent:"#00a6a6",
+    },
+  },
+  plugins: [],
+}
+
